e97f5c070612cb6c44339d00c35fe26cb3bac2ff
muxammed <muxammedsaparow@gmail.com>
2017-02-01 14:25:15 +0500

    Initial commit

---
+++ app/src/main/java/tm/mir/itpark/MainActivity.java
@@ -0,0 +1,32 @@
+package tm.mir.itpark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btntxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btntxt = (Button) findViewById(R.id.testbtn);
+        btntxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"ERKER ISHLEDI",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+
+
+}
